2bda8e9842f11f5ad71ba65041904fe2b4a06c8f
Ria Tambve <95668680+ria2003@users.noreply.github.com>
2025-07-12 01:16:40 +0530

    Update context_processors.py

--- users/context_processors.py
+++ users/context_processors.py
@@ -1,5 +1,7 @@
+# context_processors -> used to make certain variables available globally in all templates without having to pass them manually in every render() call
 def user_profile(request):
     if request.user.is_authenticated:
+        # returns dictionary with current user's profile picture URL so it can be used in any HTML template globally as {{ user_profile_pic }} eg: in navbar
         return {
             'user_profile_pic': request.user.profile_pic.url if request.user.profile_pic else None
         }
