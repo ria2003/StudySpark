6dea2baec6fd3e6103090132e17c4bd3e772c561
Ria Tambve <95668680+ria2003@users.noreply.github.com>
2025-07-12 00:40:47 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -7,6 +7,5 @@ from cloudinary.models import CloudinaryField
 class User(AbstractUser):
     about_me = models.TextField(blank=True)
     interests = models.TextField(blank=True)
-    # Replace ImageField with CloudinaryField
     profile_pic = CloudinaryField('image', default='profile_pics/default.png')
     additional_details_filled = models.BooleanField(default=False)
