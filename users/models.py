87b2c2958f40e2fa7b2de523c752ab8076dca9a8
Ria Tambve <95668680+ria2003@users.noreply.github.com>
2025-07-12 00:46:24 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,11 +1,15 @@
-# models.py
+# AbstractUser includes all standard fields (username, email, etc.), and lets you add more fields on top of it.
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# for uploading images directly to Cloudinary cloud storage instead of local storage
 from cloudinary.models import CloudinaryField
 
+# custom user model that extends AbstractUser 
 class User(AbstractUser):
     about_me = models.TextField(blank=True)
     interests = models.TextField(blank=True)
     profile_pic = CloudinaryField('image', default='profile_pics/default.png')
+
+    # boolean flag to track whether the user has filled in their bio, interests, and profile picture
     additional_details_filled = models.BooleanField(default=False)
